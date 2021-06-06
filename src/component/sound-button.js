e8f4ab1ab900035efa4c9d950550b1377a29aed0
Goker zafer <gokerlek@gmail.com>
2021-06-06 15:37:23 +0300

    button style complated

--- src/component/sound-button.js
+++ src/component/sound-button.js
@@ -1,7 +1,7 @@
 export const SoundButton = () => {
      return (
           <div className='p-1  mt-2 text-center space-x-1 space-y-2'>
-               <button className='bg-gray-900 px-5 py-5 text-sm shadow-sm font-medium tracking-wider border text-gray-100 rounded-lg hover:shadow-lg hover:bg-gray-800'>
+               <button className='bg-gray-900 px-5 py-5 text-sm shadow-sm font-medium tracking-wider border text-gray-100 rounded-lg hover:shadow-lg hover:bg-gray-800 focus:outline-none focus:ring-transparent'>
                     C
                </button>
           </div>
