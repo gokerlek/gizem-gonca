db4f7e78e1eada74524f7cdd1bd0de8d5cd3786c
Goker zafer <gokerlek@gmail.com>
2021-06-06 15:01:05 +0300

    tailwind complate

---
+++ src/component/sound-button.js
@@ -0,0 +1,9 @@
+export const SoundButton = () => {
+     return (
+          <div className='p-1  mt-2 text-center space-x-1 space-y-2'>
+               <button className='bg-gray-900 px-5 py-5 text-sm shadow-sm font-medium tracking-wider border text-gray-100 rounded-lg hover:shadow-lg hover:bg-gray-800'>
+                    Gray900
+               </button>
+          </div>
+     );
+};
