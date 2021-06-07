a57ed1c24da09c05b1a7f7614cd60820918b66d2
Goker zafer <gokerlek@gmail.com>
2021-06-08 00:36:30 +0300

    düzeltme

--- src/component/container.js
+++ src/component/container.js
@@ -1,26 +1,23 @@
 import { SoundButton } from "./sound-button";
 import { Howl } from "howler";
 
-const src1 = "sounds/D4";
-const src2 = "sounds/Cs4";
-
 const sound1 = new Howl({
-     src: [`${src1}.m4a`],
+     src: ["sounds/A4.m4a"],
 });
 const sound2 = new Howl({
-     src: [`${src2}.m4a`],
+     src: ["sounds/B4.m4a"],
 });
 
 const playSound = () => {
-     sound2.play();
      sound1.play();
+     sound2.play();
 };
 
 export const Container = () => {
      return (
           <div className='min-w-screen h-screen  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 bg-white'>
                <div className='flex flex-col w-full'>
-                    <SoundButton onClick={playSound}>C</SoundButton>
+                    <SoundButton start={playSound}>C</SoundButton>
                </div>
           </div>
      );
