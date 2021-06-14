85fcd7c4a929fee5f1529addc80909402bd50451
Goker zafer <gokerlek@gmail.com>
2021-06-14 20:47:32 +0300

    useMidiIn complated

--- src/component/container.js
+++ src/component/container.js
@@ -2,10 +2,15 @@ import { SoundButton } from "./sound-button";
 import { NoteButton } from "./note-button";
 import { Howl } from "howler";
 import sounds from "../cms/sounds.json";
+import { useState, useEffect } from "react";
+import { useMidiIn } from "./useData";
 
 const questions = sounds.questions;
-
 export const Container = () => {
+     const { velocity, midiNote, midiKeyState } = useMidiIn();
+
+     console.log(midiNote);
+
      return (
           <div className='min-w-screen h-screen  fixed  left-0 top-0 flex inset-0 z-50 bg-white '>
                <div className='flex w-full justify-center items-center'>
