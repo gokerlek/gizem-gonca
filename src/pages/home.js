cff864cb0d0abc47ecae840028348d6082cb16a1
Goker zafer <gokerlek@gmail.com>
2021-06-30 13:49:12 +0300

    virtual piano added

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,57 @@
 import { useMidiData } from "../component/use-midi-data";
 import { Container } from "../component/container";
+import { Piano, KeyboardShortcuts, MidiNumbers } from "react-piano";
+import "react-piano/dist/styles.css";
+import "../style.css";
+import { useState, useEffect } from "react";
+import { Dimension } from "react";
 
 export const Home = () => {
      const { acRef, clavinetRef } = useMidiData(
           "acoustic_grand_piano",
           "FluidR3_GM"
      );
+     console.log(acRef, clavinetRef);
 
-     return <Container></Container>;
+     const [widthData, setWidthData] = useState();
+
+     useEffect(() => {
+          setWidthData(window?.innerWidth.valueOf());
+     }, [setWidthData]);
+
+     const firstNote = MidiNumbers.fromNote("c3");
+     const lastNote = MidiNumbers.fromNote("f5");
+     const keyboardShortcuts = KeyboardShortcuts.create({
+          firstNote: firstNote,
+          lastNote: lastNote,
+          keyboardConfig: KeyboardShortcuts.HOME_ROW,
+     });
+
+     return (
+          <>
+               <Container>
+                    <Piano
+                         noteRange={{ first: firstNote, last: lastNote }}
+                         playNote={(midiNumber) => {
+                              if (clavinetRef.current?.play) {
+                                   clavinetRef.current?.play(
+                                        midiNumber,
+                                        acRef.current
+                                   );
+                              }
+                         }}
+                         stopNote={(midiNumber) => {
+                              if (clavinetRef.current?.stop) {
+                                   clavinetRef.current?.stop(
+                                        midiNumber,
+                                        acRef.current
+                                   );
+                              }
+                         }}
+                         keyboardShortcuts={keyboardShortcuts}
+                         width={1800}
+                    />
+               </Container>
+          </>
+     );
 };
