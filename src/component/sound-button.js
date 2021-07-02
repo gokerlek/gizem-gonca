9bbc91043046d1671e5c3ea8f59ac70b3194b004
Goker zafer <gokerlek@gmail.com>
2021-07-02 17:34:28 +0300

    active color

--- src/component/sound-button.js
+++ src/component/sound-button.js
@@ -2,7 +2,7 @@ import classNames from "classnames";
 
 export const SoundButton = ({ onClick, children, key, color }) => {
      const soundButton = classNames(
-          "bg-gray-100 min-w-full p-5 text-sm shadow-sm font-layout font-bold tracking-wider rounded-lg focus:outline-none focus:ring-transparent",
+          "bg-gray-100 active:bg-gray-100 min-w-full p-5 text-sm shadow-sm font-layout font-bold tracking-wider rounded-lg focus:outline-none focus:ring-transparent",
           `text-${color}`
      );
      return (
