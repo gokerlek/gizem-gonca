a1f5f3b9fef9e8ede214c6d1bc3084a8e559054f
Goker zafer <gokerlek@gmail.com>
2021-07-30 18:13:25 +0300

    toggler added and sizeme deleted and jsx download

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,9 @@
 import { useMidiData } from "../component/use-midi-data";
 import { Container } from "../component/container";
 import { Piano, KeyboardShortcuts, MidiNumbers } from "react-piano";
+import classNames from "classnames";
 import "react-piano/dist/styles.css";
-import "../style.css";
-import { SizeMe } from "react-sizeme";
+import { useState } from "react";
 
 export const Home = () => {
      const { acRef, clavinetRef } = useMidiData(
@@ -11,6 +11,15 @@ export const Home = () => {
           "FluidR3_GM"
      );
      console.log(acRef, clavinetRef);
+
+     const [toggle, setToggle] = useState(false);
+     const toggler = () => setToggle(!toggle);
+     console.log(toggle);
+     const color = () =>
+          classNames(
+               toggle ? `bg-kolay` : "bg-zor",
+               "absolute top-0 left-16 m-12 rounded-full min-w-32 p-5 text-center text-beyaz"
+          );
 
      const firstNote = MidiNumbers.fromNote("a3");
      const lastNote = MidiNumbers.fromNote("e5");
@@ -21,38 +30,214 @@ export const Home = () => {
      });
 
      return (
-          <Container>
-               <div className='min-h-screen min-w-full flex items-center'>
-                    <div className='flex min-w-full min-h-full items-center'>
-                         <SizeMe
-                              monitorHeight
-                              refreshRate={32}
-                              render={({ size }) => (
-                                   <Piano
-                                        noteRange={{
-                                             first: firstNote,
-                                             last: lastNote,
-                                        }}
-                                        playNote={(midiNumber) => {
-                                             if (clavinetRef.current?.play) {
-                                                  clavinetRef.current?.play(
-                                                       midiNumber,
-                                                       acRef.current
-                                                  );
-                                             }
-                                        }}
-                                        stopNote={() => {
-                                             if (clavinetRef.current?.stop) {
-                                                  clavinetRef.current?.stop();
-                                             }
-                                        }}
-                                        keyboardShortcuts={keyboardShortcuts}
-                                        width={size.width}
-                                   />
-                              )}
-                         />
+          <>
+               <Container>
+                    <div className='min-h-screen min-w-full flex items-center static'>
+                         <div onClick={toggler} className={color()}></div>
+
+                         <div className='flex min-w-full min-h-full items-center'>
+                              <Piano
+                                   noteRange={{
+                                        first: firstNote,
+                                        last: lastNote,
+                                   }}
+                                   playNote={(midiNumber) => {
+                                        if (clavinetRef.current?.play) {
+                                             clavinetRef.current?.play(
+                                                  midiNumber,
+                                                  acRef.current
+                                             );
+                                        }
+                                   }}
+                                   stopNote={() => {
+                                        if (clavinetRef.current?.stop) {
+                                             clavinetRef.current?.stop();
+                                        }
+                                   }}
+                                   keyboardShortcuts={keyboardShortcuts}
+                              />
+                         </div>
                     </div>
-               </div>
-          </Container>
+               </Container>
+               <style jsx>
+                    {`
+                         [data-testid="container"] {
+                              display: flex;
+                              justify-content: center;
+                         }
+                         .ReactPiano__Keyboard {
+                              position: relative;
+                              display: flex;
+                              max-width: 1500px;
+                              min-height: 500px;
+                         }
+
+                         .ReactPiano__Key--accidental {
+                              background: #009fb7;
+                              border: 1px solid #009fb7;
+                              height: 66%;
+                              box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+                         }
+
+                         .ReactPiano__Key--natural {
+                              background: #ffffff;
+                              border: 1px solid #e2e2e2;
+                              min-height: 500px;
+                              box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+                         }
+
+                         .ReactPiano__NoteLabel--accidental,
+                         .ReactPiano__NoteLabel--natural,
+                         .ReactPiano__NoteLabel--natural.ReactPiano__NoteLabel--active {
+                              color: transparent;
+                         }
+
+                         @media screen and (max-width: 1550px) {
+                              .ReactPiano__Keyboard {
+                                   max-width: 1200px;
+                                   min-height: 400px;
+                              }
+
+                              .ReactPiano__Key--natural {
+                                   min-height: 400px;
+                              }
+                         }
+                         @media screen and (max-width: 1300px) {
+                              .ReactPiano__Keyboard {
+                                   max-width: 1000px;
+                                   min-height: 330px;
+                              }
+
+                              .ReactPiano__Key--natural {
+                                   min-height: 330px;
+                              }
+                         }
+                         @media screen and (max-width: 1100px) {
+                              .ReactPiano__Keyboard {
+                                   max-width: 750px;
+                                   min-height: 240px;
+                              }
+
+                              .ReactPiano__Key--natural {
+                                   min-height: 240px;
+                              }
+                         }
+                         @media screen and (max-width: 850px) {
+                              .ReactPiano__Keyboard {
+                                   max-width: 540px;
+                                   min-height: 180px;
+                              }
+
+                              .ReactPiano__Key--natural {
+                                   min-height: 180px;
+                              }
+                         }
+                         @media screen and (max-width: 640px) {
+                              .ReactPiano__Keyboard {
+                                   max-width: 320px;
+                                   min-height: 108px;
+                              }
+
+                              .ReactPiano__Key--natural {
+                                   min-height: 108px;
+                              }
+                         }
+                    `}
+               </style>
+               {toggle ? (
+                    <style jsx>
+                         {`
+                              .ReactPiano__Key--active.ReactPiano__Key--accidental {
+                                   border: 1px solid #3ac8da;
+                                   border-top: 1px solid #3ac8da;
+                                   height: 65%;
+                              }
+                              .ReactPiano__Key--active {
+                                   background: #3ac8da;
+                              }
+
+                              .ReactPiano__Key--active.ReactPiano__Key--natural {
+                                   border: 1px solid #3ac8da;
+                                   min-height: 498px;
+                              }
+                              @media screen and (max-width: 1550px) {
+                                   .ReactPiano__Key--active.ReactPiano__Key--natural {
+                                        min-height: 398px;
+                                   }
+                              }
+                              @media screen and (max-width: 1300px) {
+                                   .ReactPiano__Key--active.ReactPiano__Key--natural {
+                                        min-height: 328px;
+                                   }
+                              }
+                              @media screen and (max-width: 1100px) {
+                                   .ReactPiano__Key--active.ReactPiano__Key--natural {
+                                        min-height: 238px;
+                                   }
+                              }
+                              @media screen and (max-width: 850px) {
+                                   .ReactPiano__Key--active.ReactPiano__Key--natural {
+                                        min-height: 178px;
+                                   }
+                              }
+                              @media screen and (max-width: 640px) {
+                                   .ReactPiano__Key--active.ReactPiano__Key--natural {
+                                        min-height: 106px;
+                                   }
+                              }
+                         `}
+                    </style>
+               ) : (
+                    <style jsx>
+                         {`
+                              .ReactPiano__Key--active.ReactPiano__Key--accidental {
+                                   border: 1px solid transparent;
+                                   border-top: 1px solid transparent;
+                                   height: 66%;
+                              }
+                              .ReactPiano__Key--active {
+                                   background: #red;
+                              }
+
+                              .ReactPiano__Key--active.ReactPiano__Key--natural {
+                                   border: 1px solid #e2e2e2;
+                                   min-height: 500px;
+                                   background: #fff;
+                              }
+                              .ReactPiano__NoteLabel--accidental,
+                              .ReactPiano__NoteLabel--natural,
+                              .ReactPiano__NoteLabel--natural.ReactPiano__NoteLabel--active {
+                                   color: transparent;
+                              }
+
+                              @media screen and (max-width: 1550px) {
+                                   .ReactPiano__Key--active.ReactPiano__Key--natural {
+                                        min-height: 400px;
+                                   }
+                              }
+                              @media screen and (max-width: 1300px) {
+                                   .ReactPiano__Key--active.ReactPiano__Key--natural {
+                                        min-height: 330px;
+                                   }
+                              }
+                              @media screen and (max-width: 1100px) {
+                                   .ReactPiano__Key--active.ReactPiano__Key--natural {
+                                        min-height: 240px;
+                                   }
+                              }
+                              @media screen and (max-width: 850px) {
+                                   .ReactPiano__Key--active.ReactPiano__Key--natural {
+                                        min-height: 180px;
+                                   }
+                              }
+                              @media screen and (max-width: 640px) {
+                                   .ReactPiano__Key--active.ReactPiano__Key--natural {
+                                        min-height: 106px;
+                                   }
+                              }
+                         `}
+                    </style>
+               )}
+          </>
      );
 };
